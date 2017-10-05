a7047829d7a5e66e5cccdfe7db127e6f69ca2a5d
Florida <halleck.g.florida@gmail.com>
2017-10-05 09:48:54 +0200

    ahorcado

---
+++ Js/ahorcado.js
@@ -0,0 +1,11 @@
+// script Juego Ahorcado
+// esta variable almacena una letra
+var adivinar="h";
+//muestra en pantalla un prompt con un texto y un campo a rellenar
+var n1 = prompt("Adivina la letra");
+//comprueba si adivinar es igual a n1 y muestra el alert de "es correcto"
+if (adivinar==n1) {
+alert("Es correcto!!")}
+//comprueba si adivinar es idiferente a n1 y muestra el alert de "No has acertado"
+if (adivinar!=n1) {
+alert("No has acertado")}
